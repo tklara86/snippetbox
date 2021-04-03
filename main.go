d0a9712c595448a3470a9011909b16685c076704
Tomasz Klara <tomaszklara@gmail.com>
2021-04-03 17:07:00 +0100

    customizing HTTP Headers

--- main.go
+++ main.go
@@ -13,10 +13,43 @@ import (
 // Home Handler
 func Home(w http.ResponseWriter, r *http.Request) {
 
+	if r.URL.Path != "/" {
+		// replies with 404 HTTP status error
+		http.NotFound(w, r)
+		return
+	}
 
-	_, err := w.Write([]byte("Hello form SnippetBox"))
+	_, err := w.Write([]byte("Hello from SnippetBox"))
 	if err != nil {
 		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+}
+
+// ShowSnippet handler function
+func ShowSnippet(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Display specific snippets"))
+	if err != nil {
+		http.Error(w, "could not get a snippet", http.StatusBadRequest)
+	}
+}
+// CreateSnippet handler function
+func CreateSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(405)
+		_, err := w.Write([]byte("Method Not Allowed"))
+
+		if err != nil {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		}
+		return
+	}
+	_, err := w.Write([]byte(`{"msg": "Create a new snippet"}`))
+	if err != nil {
+		http.Error(w, "could not create a snippet", http.StatusBadRequest)
 	}
 }
 
@@ -25,6 +58,8 @@ func main() {
 	// router - servemux
 	sm := http.NewServeMux()
 	sm.HandleFunc("/", Home)
+	sm.HandleFunc("/snippet", ShowSnippet)
+	sm.HandleFunc("/snippet/create", CreateSnippet)
 
 	srv := http.Server{
 		Addr: ":8080",
