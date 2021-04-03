f3f35a1b3004c42ef0c122e5eef63a6dc5d3c71e
Tomasz Klara <tomaszklara@gmail.com>
2021-04-03 14:30:29 +0100

    added graceful shutdown

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 
 // Home Handler
 func Home(w http.ResponseWriter, r *http.Request) {
+
+
 	_, err := w.Write([]byte("Hello form SnippetBox"))
 	if err != nil {
 		http.Error(w, "Bad request", http.StatusBadRequest)
@@ -15,14 +21,35 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	l := log.New(os.Stdout, "snippetbox-", log.LstdFlags)
+	// router - servemux
 	sm := http.NewServeMux()
 	sm.HandleFunc("/", Home)
 
 	srv := http.Server{
 		Addr: ":8080",
 		Handler: sm,
+		ErrorLog: l,
 	}
 
-	err := srv.ListenAndServe()
-	log.Fatal(err)
+	// start the server
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	// Graceful shutdown
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	_ = srv.Shutdown(ctx)
 }
