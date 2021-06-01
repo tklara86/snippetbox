97d58c8f1370899d7aed609f43e1885ec43a8cda
Tomasz Klara <tomaszklara@gmail.com>
2021-06-01 22:40:14 +0100

    added connection to mysql database, godotenv

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -13,12 +18,33 @@ type application struct {
 }
 
 func main() {
+
+	// load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
 	// address flag
 	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// define DSN string
+	dsn := flag.String("dsn", fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUser, dbPassword, dbHost, dbName), "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.LstdFlags)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.LstdFlags |log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		infoLog: infoLog,
@@ -32,10 +58,23 @@ func main() {
 	}
 	colorizeTerminalMsg(ColorGreen)
 	infoLog.Printf("Started server on port %s", srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 
 	if err != nil {
 		colorizeTerminalMsg(ColorRed)
 		errorLog.Fatal(err)
 	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	// opens database
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	// Verifies connection to the database is still alive
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/tklara86
 
 go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+)
